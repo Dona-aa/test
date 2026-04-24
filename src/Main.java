b6615fc4a79042674f9269993a47c8d150129f09
Dona-aa <edonaperaj4@gmail.com>
2026-04-24 11:24:43 +0200

    message

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,6 @@
 public class Main {
     public static void main(String[] args){
         System.out.println("Hallo Projektleiterin!");
+        System.out.println("Ich bin Edona, die Projektleiterin!");
     }
 }
